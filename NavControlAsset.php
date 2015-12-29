48ddf5c8e03a25805dac3bc59588f7d49e2c9f53
Bob Gunion <rfgunion@lbl.gov>
2015-12-29 15:51:26 -0500

    Added alias to NavControlAsset

--- NavControlAsset.php
+++ NavControlAsset.php
@@ -6,7 +6,12 @@ use yii\web\AssetBundle;
 
 class NavControlAsset extends AssetBundle {
 	
-	public $sourcePath = 'dudonsky/navcontrol/assets';
+	public function init() {
+		\Yii::setAlias('@navcontrol', __DIR__);
+		return parent::init();
+	}
+
+	public $sourcePath = '@navcontrol/assets';
 	public $css = [];
 	public $js = [
 		'jquery.flot.navigationControl.js',
